f496de31fafcb5a06c3f3ea90e52d00a435dcf42
Grazziano <grazzianofagundes@gmail.com>
2021-03-16 14:57:31 -0300

    Voting HTML

---
+++ resources/views/voting.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+@section('content')
+    <div class="container">
+        <form action="">
+            <fieldset class="form-group">
+                <div class="col-sm-10"></div>
+            </fieldset>
+        </form>
+    </div>
+@endsection
