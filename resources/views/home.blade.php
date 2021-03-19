4f44679e36d7bba61d39639827fcd5bd6f4b98d3
Grazziano <grazzianofagundes@gmail.com>
2021-03-19 13:19:12 -0300

    Porcentage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
                 <div class="card-body">
                     Candidate name: {{ $candidate->name }}
                     <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: {{ ($candidate->votes / 1000) * 100 }}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="1000">{{ $candidate->votes }}%</div>
+                        <div class="progress-bar" role="progressbar" style="width: {{ ($candidate->votes / 10000) * 100 }}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="10000">{{ ($candidate->votes / 10000) * 100 }}%</div>
                     </div>
 
                 </div>
