7855755b3881e5e2386418c9b9b7347c023eec9a
Grazziano <grazzianofagundes@gmail.com>
2021-03-19 21:01:20 -0300

    Information column

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,12 @@
             <div class="card mb-3 mt-3">
                 <div class="card-body">
                     Candidate name: {{ $candidate->name }}
+
+                    <details class="mt-3">
+                        <summary>Get to know this candidate</summary>
+                        <p>{{ $candidate->information }}</p>
+                    </details>
+
                     <div class="progress">
                         <div class="progress-bar" role="progressbar" style="width: {{ ($candidate->votes / 10000) * 100 }}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="10000">{{ ($candidate->votes / 10000) * 100 }}%</div>
                     </div>
